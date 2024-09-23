1cbc4f9d692ebf89a7e6787409d7bb65287adb8c
Livio Mendonca <62294315+liviomendonca@users.noreply.github.com>
2024-09-23 20:31:06 +0000

    quebra o teste para testar ruleset

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -20,7 +20,7 @@ test("GET to /api/v1/status should return db info", async () => {
   const responseBody = await response.json();
 
   expect(responseBody.dependencies.database.version).toBeDefined();
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.version).toEqual("17.0");
 
   expect(responseBody.dependencies.database.max_connections).toBeDefined();
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
